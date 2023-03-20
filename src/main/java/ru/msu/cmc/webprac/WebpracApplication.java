d93156317044ae82772896a1b1555ce87e89cd94
Egor Mayorov <mayorov.egor.130602@gmail.com>
2023-03-21 01:37:27 +0300

    main class updated (little test done)

--- src/main/java/ru/msu/cmc/webprac/WebpracApplication.java
+++ src/main/java/ru/msu/cmc/webprac/WebpracApplication.java
@@ -2,12 +2,22 @@ package ru.msu.cmc.webprac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.msu.cmc.webprac.tables.Reader;
+import ru.msu.cmc.webprac.utils.DAOFactory;
+import java.util.List;
 
 @SpringBootApplication
 public class WebpracApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebpracApplication.class, args);
+		//System.out.println("AAAAAa");
+		try {
+			SpringApplication.run(WebpracApplication.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//System.out.println("AAAAAa");
+		List<Reader> oneName = DAOFactory.getInstance().getReaderDAO().getReaderBySurame("Пивоваров");
+		System.out.println(oneName);
 	}
-
 }
