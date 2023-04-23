8253a0a5e0f856adcdb1ab19e6a42fb8a3c91e2e
Egor Mayorov <mayorov.egor.130602@gmail.com>
2023-04-23 20:52:56 +0300

    sql updated

--- src/resources/Create_tables.sql
+++ src/resources/Create_tables.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS books, book_copy, records, reader;
 
 CREATE TABLE books (
-                         "book_id" int,
+                         "book_id" SERIAL,
                          "name" text not null ,
                          "author" text not null ,
                          "publishing_house" text not null ,
@@ -14,12 +14,12 @@ CREATE TABLE books (
 
 CREATE TABLE book_copy (
                              "book_id" int not null ,
-                             "copy_id" int,
+                             "copy_id" SERIAL,
                              PRIMARY KEY ("copy_id")
 );
 
 CREATE TABLE records (
-                           "record_id" int,
+                           "record_id" SERIAL,
                            "reader_id" int not null ,
                            "copy_id" int not null ,
                            "taking_date" date not null ,
@@ -28,7 +28,7 @@ CREATE TABLE records (
 );
 
 CREATE TABLE reader (
-                          "reader_id" int,
+                          "reader_id" SERIAL,
                           "name" text not null ,
                           "surname" text not null ,
                           "patronymic" text,
