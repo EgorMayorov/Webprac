a7e42f41ebdf3c2695289371552b3d7a86817be3
Egor Mayorov <mayorov.egor.130602@gmail.com>
2023-04-23 23:37:09 +0300

    some new hand made tests in main

--- src/main/java/ru/msu/cmc/webprac/WebpracApplication.java
+++ src/main/java/ru/msu/cmc/webprac/WebpracApplication.java
@@ -18,21 +18,23 @@ public class WebpracApplication {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		Reader oneName = DAOFactory.getInstance().getReaderDAO().getReaderBySurname("Пивоваров");
-		System.out.println(oneName);
-		List<Books> books = DAOFactory.getInstance().getReaderDAO().getReaderBooksBySurname("Пивоваров");
-		for(Books b:books) {
-			System.out.println(b.toString());
-		}
+
+//		Reader oneName = DAOFactory.getInstance().getReaderDAO().getReaderBySurname("Пивоваров");
+//		System.out.println(oneName);
+//		List<Books> books = DAOFactory.getInstance().getReaderDAO().getReaderBooksBySurname("Пивоваров");
+//		for(Books b:books) {
+//			System.out.println(b.toString());
+//		}
+
 //		DAOFactory.getInstance().getReaderDAO().addReader(new Reader("Михаил", "Кочармин",
 //				"Дмитриевич", "23-04-2023", "28-09-2002",
 //				"ДСЛ МГУ", "879150000000"));
 
-		DAOFactory.getInstance().getBooksDAO().addBook(new Books("Контакт", "Карл Саган",
-				"Альпина нон-фикшн", 10, "роман", "21-04-2023"));
+//		DAOFactory.getInstance().getBooksDAO().addBook(new Books("Контакт", "Карл Саган",
+//				"Альпина нон-фикшн", 10, "роман", "21-04-2023"));
 
 //		Books book = DAOFactory.getInstance().getBooksDAO().getBookByName("Контакт");
-//		book.setAmount(15);
+//		book.setAbout("Олег сказал, что книга крутая!");
 //		DAOFactory.getInstance().getBooksDAO().updateBook(book);
 
 //		Reader reader = DAOFactory.getInstance().getReaderDAO().getReaderBySurname("Кочармин");
@@ -44,5 +46,19 @@ public class WebpracApplication {
 //
 //		Reader reader = DAOFactory.getInstance().getReaderDAO().getReaderBySurname("Кочармин");
 //		DAOFactory.getInstance().getReaderDAO().deleteReader(reader);
+
+//		DAOFactory.getInstance().getRecordsDAO().addRecord(new Records("Кочармин", "Контакт"));
+
+		List<Books> books1 = DAOFactory.getInstance().getReaderDAO().getReaderBooksBySurname("Кочармин");
+		for(Books b:books1) {
+			System.out.println(b.toString());
+		}
+
+//		DAOFactory.getInstance().getRecordsDAO().returnBook("Кочармин", "Контакт");
+
+		List<Reader> readers = DAOFactory.getInstance().getBooksDAO().getReadersByBook("Приключения Эраста Фандорина. Азазель");
+		for(Reader r:readers) {
+			System.out.println(r.toString());
+		}
 	}
 }
