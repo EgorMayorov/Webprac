a6bf8373b21fe8f96e1d573bb0c0bb6774e9a308
Egor Mayorov <mayorov.egor.130602@gmail.com>
2023-04-24 00:17:00 +0300

    first test

--- src/test/java/ru/msu/cmc/webprac/tests/WebpracApplicationTests.java
+++ src/test/java/ru/msu/cmc/webprac/tests/WebpracApplicationTests.java
@@ -1,7 +1,15 @@
 package ru.msu.cmc.webprac.tests;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.msu.cmc.webprac.tables.Reader;
+import ru.msu.cmc.webprac.utils.DAOFactory;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class WebpracApplicationTests {
@@ -10,4 +18,12 @@ class WebpracApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void addReaderTest () {
+		DAOFactory.getInstance().getReaderDAO().addReader(new Reader("Михаил", "Кочармин",
+				"Дмитриевич", "23-04-2023", "28-09-2002",
+				"ДСЛ МГУ", "879150000000"));
+		Reader reader = DAOFactory.getInstance().getReaderDAO().getReaderBySurname("Кочармин");
+		assertNotNull(reader);
+	}
 }
