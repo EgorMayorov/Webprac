8baf7c0b5a22b0b8c6499c1cd0a1b48686c5c287
Egor Mayorov <mayorov.egor.130602@gmail.com>
2023-05-19 01:10:01 +0300

    new functions for first pages

--- src/main/java/ru/msu/cmc/webprac/MainController.java
+++ src/main/java/ru/msu/cmc/webprac/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.msu.cmc.webprac.DAO.ReaderDAO;
 import ru.msu.cmc.webprac.DAO.impl.ReaderDAOImpl;
 import ru.msu.cmc.webprac.tables.Reader;
@@ -26,6 +28,18 @@ public class MainController {
         List<Reader> readers = DAOFactory.getInstance().getReaderDAO().getAllReader();
         model.addAttribute("readerList", readers);
 //        model.addAttribute("ReaderDAO", readerDAO);
+        return "readers";
+    }
+
+    @PostMapping("/readers")
+    public String search(Model model, @RequestParam(name = "search_reader", required = false) String search_reader){
+        if (search_reader.equals("")) {
+            List<Reader> reader = DAOFactory.getInstance().getReaderDAO().getAllReader();
+            model.addAttribute("readerList", reader);
+        } else {
+            Reader reader = DAOFactory.getInstance().getReaderDAO().getReaderBySurname(search_reader);
+            model.addAttribute("readerList", reader);
+        }
         return "readers";
     }
 }
