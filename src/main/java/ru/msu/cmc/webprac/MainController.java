93b6cf67c7431e0e09317199e8caf71ca6c60910
Egor Mayorov <mayorov.egor.130602@gmail.com>
2023-05-20 03:15:11 +0300

    new pages and new functionality adeed

--- src/main/java/ru/msu/cmc/webprac/MainController.java
+++ src/main/java/ru/msu/cmc/webprac/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.msu.cmc.webprac.DAO.ReaderDAO;
 import ru.msu.cmc.webprac.DAO.impl.ReaderDAOImpl;
+import ru.msu.cmc.webprac.tables.Books;
 import ru.msu.cmc.webprac.tables.Reader;
 import ru.msu.cmc.webprac.utils.DAOFactory;
 
@@ -41,5 +42,19 @@ public class MainController {
             model.addAttribute("readerList", reader);
         }
         return "readers";
+    }
+
+    @PostMapping("/reader_info")
+    public String readerInfo (Model model, @RequestParam(name = "reader_surname", required = false) String reader_surname) {
+        Reader reader = DAOFactory.getInstance().getReaderDAO().getReaderBySurname(reader_surname);
+        model.addAttribute("reader_info", reader);
+        return "reader_info";
+    }
+
+    @PostMapping("/reader_books")
+    public String readerBooks (Model model, @RequestParam(name = "reader_surname", required = false) String reader_surname) {
+        List<Books> books = DAOFactory.getInstance().getReaderDAO().getReaderBooksBySurname(reader_surname);
+        model.addAttribute("booksList", books);
+        return "reader_books";
     }
 }
